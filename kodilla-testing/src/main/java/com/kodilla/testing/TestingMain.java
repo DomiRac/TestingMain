39633c0cf75c72a4cb3243c1d19ddaaedee6f1ea
“DomiRac <dominika.rac@gmail.com>
2018-04-16 13:22:14 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.Calculator;
+import com.kodilla.testing.User.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("System OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test- pierwszy test jednostkowy");
+        Calculator calculator = new Calculator();
+
+        int result1 = calculator.add(4, 9);
+        System.out.println(result1);
+        int result2 = calculator.substract(6, 4);
+        System.out.println(result2);
     }
 }
