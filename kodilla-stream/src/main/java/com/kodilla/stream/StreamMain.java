03ee1d81bc93f45acc91dcd4636248f02d508659
“DomiRac <dominika.rac@gmail.com>
2018-05-09 17:38:49 +0200

    text

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.stream;
 
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Kittieeeees", (a -> a.toUpperCase()));
+        poemBeautifier.beautify("Kittieeeees", (a -> a + "are so sweeeet"));
+        poemBeautifier.beautify("Kittieeeees!", ( a -> "WHAAAAT? " + a));
+        poemBeautifier.beautify("Kittieeeees", (a -> String.valueOf(a.length())));
+        poemBeautifier.beautify("Kittieeeees", ( a -> a + a + a + a));
+        poemBeautifier.beautify("Kittieeeees", (a -> a.replace('i', '!')));
     }
 }
