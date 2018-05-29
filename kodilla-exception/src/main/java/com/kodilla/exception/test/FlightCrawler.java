5e6657f5519f77feb87fce457219464ad4015b53
“DomiRac <dominika.rac@gmail.com>
2018-05-29 17:01:54 +0200

    txt

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightCrawler.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightCrawler.java
@@ -1,13 +1,16 @@
-package com.kodilla.exception.test;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class FlightCrawler {
-    public Map<String, Boolean> findFlight(Flight flight) throws RouteNotFoundException {
-        HashMap<String, Boolean> flights = new HashMap<>();
-        flights.put("Warsaw Chopin Airport", true);
-        flights.put("Doha Aiport", true);
-        return flights;
-    }
-}
+//package com.kodilla.exception.test;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class FlightCrawler {
+//    public Map<String, Boolean> findFlight(Flight flight) throws RouteNotFoundException {
+//        HashMap<String, Boolean> flights = new HashMap<>();
+//        if (flights.containsKey(flight.getArrivalAirport())) {
+//            System.out.println("You can fly from: " + flight.getArrivalAirport() + "to: " + flight.getDepartureAirport());
+//        } else {
+//            throw new RouteNotFoundException("Sorry, there is no flight");
+//        }
+//        return
+//    }
+//}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Stream.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Stream.java
@@ -2,8 +2,8 @@ package com.kodilla.good.patterns.challenges;
 
 public class Stream {
     public static void main(String[]args) {
-        MovieStore.getMovies().stream()
-                .map(s -> s.replaceAll(" ", "!"))
-                .forEach(System.out::println);
+        MovieStore.getMovies().entrySet().stream()
+                .map(d -> d.toString().replaceAll(" ", "!"))
+                .forEach(System.out::print);
     }
 }
