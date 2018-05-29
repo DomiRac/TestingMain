cb5a7ee08099bf5c949019d3b3ccc4877895ee2c
“DomiRac <dominika.rac@gmail.com>
2018-05-29 17:06:30 +0200

    txt

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightCrawler.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightCrawler.java
@@ -1,16 +1,16 @@
-//package com.kodilla.exception.test;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class FlightCrawler {
-//    public Map<String, Boolean> findFlight(Flight flight) throws RouteNotFoundException {
-//        HashMap<String, Boolean> flights = new HashMap<>();
-//        if (flights.containsKey(flight.getArrivalAirport())) {
-//            System.out.println("You can fly from: " + flight.getArrivalAirport() + "to: " + flight.getDepartureAirport());
-//        } else {
-//            throw new RouteNotFoundException("Sorry, there is no flight");
-//        }
-//        return
-//    }
-//}
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightCrawler {
+    public Map<String, Boolean> findFlight(Flight flight) throws RouteNotFoundException {
+        HashMap<String, Boolean> flights = new HashMap<>();
+        if (flights.containsKey(flight.getArrivalAirport())) {
+            System.out.println("You can fly from: " + flight.getArrivalAirport() + "to: " + flight.getDepartureAirport());
+        } else {
+            throw new RouteNotFoundException("Sorry, there is no flight");
+        }
+        return flights;
+    }
+}
